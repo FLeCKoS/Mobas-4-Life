079293d5ef7fb72ac657bf8dcf2798c4362932b8
JosephJP710 <89850135+FLeCKoS@users.noreply.github.com>
2021-12-05 09:54:21 -0500

    Actualizacion
    05/12

---
+++ Js/script.js
@@ -0,0 +1,43 @@
+document.getElementById("nav_def").addEventListener("click", mostrar_definicion);
+document.getElementById("btn_i_def").addEventListener("click", ocultar_definicion);
+document.getElementById("nav_moba").addEventListener("click", mostrar_moba);
+document.getElementById("btn_i_moba").addEventListener("click", ocultar_moba);
+document.getElementById("nav_rol").addEventListener("click", mostrar_roles);
+document.getElementById("btn_i_roles").addEventListener("click", ocultar_roles);
+document.getElementById("nav_otr").addEventListener("click", mostrar_otr_roles);
+document.getElementById("btn_i_otros").addEventListener("click", ocultar_otr_roles);
+
+function mostrar_definicion() {
+    Def_moba.style.display = "block";
+}
+
+function ocultar_definicion() {
+    Def_moba.style.display = "none";
+}
+
+function mostrar_moba() {
+    Princ_moba.style.display = "block";
+
+}
+
+function ocultar_moba() {
+    Princ_moba.style.display = "none";
+}
+
+function mostrar_roles() {
+    Roles.style.display = "block";
+
+}
+
+function ocultar_roles() {
+    Roles.style.display = "none";
+}
+
+function mostrar_otr_roles() {
+    otr_roles.style.display = "block";
+
+}
+
+function ocultar_otr_roles() {
+    otr_roles.style.display = "none";
+}
