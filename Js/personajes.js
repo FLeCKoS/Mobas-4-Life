0cf8908334278cdabe69921e500a7f5f3a53aea3
JosephJP710 <89850135+FLeCKoS@users.noreply.github.com>
2021-12-17 23:25:59 -0500

    actualización js
    17/12/2021

--- Js/personajes.js
+++ Js/personajes.js
@@ -3,6 +3,18 @@ var vEmail = "";
 var vTelefono = "";
 
 
+var obtenersrc = document.getElementById('osrc1'),
+    src = obtenersrc.getElementsByTagName('img')[0].src;
+
+/* alert(src); */
+
+//http://127.0.0.1:5500/IMG/Wild%20rift/Ahri_ChampThumb.jpg
+
+let textosrc = src;
+let result = textosrc.replace("http://127.0.0.1:5500","..");
+
+
+
 function mostarAlert() {
 
     var validaInput = true;
@@ -33,24 +45,106 @@ function mostarAlert() {
     }
 }
 
+function CopiarImagen_ocho(){
+
+    var tempImg = $("#Imagen_ocho").attr("src");
+    var imgcard = $("#img_temp").attr("src");
+  
+    $("#img_temp").attr("src", tempImg);
+  
+}
+
+function CopiarImagen_seis(){
+
+    var tempImg = $("#Imagen_seis").attr("src");
+    var imgcard = $("#img_temp").attr("src");
+  
+    $("#img_temp").attr("src", tempImg);
+  
+}
+
+function CopiarImagen_siete(){
+
+    var tempImg = $("#Imagen_siete").attr("src");
+    var imgcard = $("#img_temp").attr("src");
+    
+  
+    $("#img_temp").attr("src", tempImg);
+  
+}
+
+function CopiarImagen_cinco(){
+
+    var tempImg = $("#Imagen_cinco").attr("src");
+    var imgcard = $("#img_temp").attr("src");
+    
+  
+    $("#img_temp").attr("src", tempImg);
+  
+}
+
+function CopiarImagen_cuatro(){
+
+    var tempImg = $("#Imagen_cuatro").attr("src");
+    var imgcard = $("#img_temp").attr("src");
+    
+  
+    $("#img_temp").attr("src", tempImg);
+  
+}
+
+function CopiarImagen_tres(){
+
+    var tempImg = $("#Imagen_tres").attr("src");
+    var imgcard = $("#img_temp").attr("src");
+    
+  
+    $("#img_temp").attr("src", tempImg);
+  
+}
+
+function CopiarImagen_dos(){
+
+    var tempImg = $("#Imagen_dos").attr("src");
+    var imgcard = $("#img_temp").attr("src");
+    
+  
+    $("#img_temp").attr("src", tempImg);
+  
+}
+
+function CopiarImagen_uno(){
+
+    var tempImg = $("#Imagen_uno").attr("src");
+    var imgcard = $("#img_temp").attr("src");
+    
+  
+    $("#img_temp").attr("src", tempImg);
+  
+}
+
+
+
+
 function fnCrearTarjetas() {
     vNombre = document.getElementById("nombre").value;
     vEmail = document.getElementById("email").value;
     vTelefono = document.getElementById("telefono").value;
+    var imgcard = $("#img_temp").attr("src");
 
 
 
-    var imgTarjeta = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSMEki8yUD2ErCLg6yZUIgTDAphf9i43vlmUP-dGMxQttnJ4ZPjDgH3Oa_2mYO5DVpm1o8&usqp=CAU";
+    var imgTarjeta = imgcard;
     var titleTarjeta = "Nueva Tarjeta";
     var textTarjeta = "Some quick example text to build on the card title and make up the bulk of the card's content.";
 
-    var objetoHtml = `<div class="card" style="width: 19rem; margin-top:15px; box-shadow: rgba(0, 0, 0, 0.16) 0px 1px 4px, rgb(51, 51, 51) 0px 0px 0px 3px;">` +
-        `<img src="` + imgTarjeta + `" class="card-img-top" alt="...">` +
-        `<div class="card-body">` +
-        `<h5 class="card-title">` + vNombre + `</h5>` +
-        `<p class="card-text" style="color:black;">` + vEmail + `</p>` +
-        `<p class="card-text" style="color:black;">` + vTelefono + `</p>` +
-        `<a href="../Personajes/Arena of Valor.html" class="btn btn-primary">Refrescar</a>` +
+    var objetoHtml = `<div class="crear_card" style="width: 19rem; margin-top:15px; box-shadow: rgba(0, 0, 0, 0.16) 0px 1px 4px, rgb(51, 51, 51) 0px 0px 0px 3px;">` +
+        `<img src="` + imgTarjeta + `" class="img_card" alt="..." >` +
+        `<div class="card_body">` +
+        `<h3 class="card-title">`+ "Usuario: " + vNombre + `</h3>` +
+        `<p class="card_text">`+ "Email: " + vEmail + `</p>` +
+        `<p class="card_text">`+ "Comentario: " + vTelefono + `</p>` +
+        
         `</div>` +
         `</div>`;
 
